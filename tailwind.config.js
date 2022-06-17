78598950e2482fc1686ea2ca265f9d2063afdd07
Ameer <ameeralichandio407@gmail.com>
2022-06-17 18:10:11 +0500

    dashboard completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["*"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'Clubby': 'Pacifico'
+            }
+        },
+    },
+    plugins: [],
+}
